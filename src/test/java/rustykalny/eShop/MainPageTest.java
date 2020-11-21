46b95c8840eb690fd66a429fc9a33a8951b8030f
CezG <cezarygulba@gmail.com>
2020-11-21 22:50:42 +0100

    first driver without setting source on hard disk

--- src/test/java/rustykalny/eShop/MainPageTest.java
+++ src/test/java/rustykalny/eShop/MainPageTest.java
@@ -1,10 +1,23 @@
 package rustykalny.eShop;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class MainPageTest {
+
+
+
+//    @Before
+//    public void setupTest() {
+//        driver = new ChromeDriver();
+//    }
     @Test
     public void openPage() {
-
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.wp.pl");
     }
 }
